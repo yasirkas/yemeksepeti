8bb697917f9c36f5e030e93c1bbe46faf2af0650
Yasir Kaş <yasirkas77@hotmail.com>
2022-11-20 18:23:41 +0300

    product modeli olusturuldu ve product-category iliski saglandi

--- yemeksepeti/product/admin.py
+++ yemeksepeti/product/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-
-from .models import Category
-
+from .models import Category, Product
 # Register your models here.
-
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
 
-admin.site.register(Category,CategoryAdmin)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'price', 'amount', 'status']
+    list_filter = ['status', 'category']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- yemeksepeti/product/models.py
+++ yemeksepeti/product/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Category(models.Model):
     STATUS = {
-        ('True','Evet'),
-        ('False','Hayır'),
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
     }
     title = models.CharField(max_length=200)
     keywords = models.CharField(max_length=250)
@@ -13,7 +13,28 @@ class Category(models.Model):
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
     slug = models.SlugField()
-    parent = models.ForeignKey('self', blank=True, null=True, related_name='children',on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+# Product class olusturuldu.
+class Product(models.Model):
+    STATUS = {
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    }
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    keywords = models.CharField(max_length=250)
+    description = models.DateTimeField(max_length=200)
+    image = models.ImageField(blank=True, upload_to='images/')
+    price = models.FloatField()
+    amount = models.IntegerField()
+    detail = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS)
+    slug = models.SlugField()
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
