8f0af09718bdecc502de91b4125784bbfab53ff1
Yasir Kaş <yasirkas77@hotmail.com>
2022-11-20 16:40:45 +0300

    superuser olusturuldu

--- yemeksepeti/product/models.py
+++ yemeksepeti/product/models.py
@@ -16,3 +16,6 @@ class Category(models.Model):
     parent = models.ForeignKey('self', blank=True, null=True, related_name='children',on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
