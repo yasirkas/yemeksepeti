892b5fe95d091487d26c8d6746ab8f4b349e7487
Yasir Kaş <yasirkas77@hotmail.com>
2022-12-25 00:57:37 +0300

    template ici islemler yapildi
    yazılar türkçeleştirildi
    türkiye logosu eklendi
    yemek logoları eklendi
    kategoriler türk restoranlarıyla değiştirildi

--- yemeksepeti/home/views.py
+++ yemeksepeti/home/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    text="Django Kurulumu: python -m pip install -e django <br> Proje Oluşturma: django-admin startproject mysite <br>" \
-         " App Ekleme: python manage.my startapp polls"
+    text="Ana Sayfa"
     context = {'text': text}
     return render(request, 'index.html', context)
