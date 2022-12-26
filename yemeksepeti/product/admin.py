f7b3bcbee3ffb1dda84a8c7e1633fda3e82b924c
Yasir Kaş <yasirkas77@hotmail.com>
2022-12-26 15:50:22 +0300

    product admin resim galerisi olusturuldu

--- yemeksepeti/product/admin.py
+++ yemeksepeti/product/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Images
 # Register your models here.
+
+class ProductImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
@@ -8,6 +13,11 @@ class CategoryAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'price', 'amount', 'status']
     list_filter = ['status', 'category']
+    inlines = [ProductImageInline]
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'product','image']
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Images,ImagesAdmin)
--- yemeksepeti/product/models.py
+++ yemeksepeti/product/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
         ('False', 'Hayır'),
     }
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
     keywords = models.CharField(max_length=250)
     description = models.DateTimeField(max_length=200)
     image = models.ImageField(blank=True, upload_to='images/')
@@ -37,6 +37,14 @@ class Product(models.Model):
     slug = models.SlugField()
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Images(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100,blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
 
     def __str__(self):
         return self.title
